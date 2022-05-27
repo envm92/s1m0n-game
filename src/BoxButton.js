1e96c016f9df8870b34e0d8ec112076ee9652080
Erika Valdes <erimimi@erikas-mbp.lan>
2022-05-26 21:30:21 -0700

    Release v0.0.3

--- src/BoxButton.js
+++ src/BoxButton.js
@@ -11,7 +11,6 @@ export class BoxButton extends LitElement {
         box-shadow: 5px 5px 5px 0 rgba(0,0,0,0.42);
         -webkit-box-shadow: 5px 5px 5px 0 rgba(0,0,0,0.42);
         -moz-box-shadow: 5px 5px 5px 0 rgba(0,0,0,0.42);
-        background: var(--background);
       }
       :host([enabled='true']) {
         cursor: pointer;
@@ -35,6 +34,31 @@ export class BoxButton extends LitElement {
         50%{ -ms-transform: scale(1.2); transform: scale(1.2); }
         100%{ -ms-transform: scale(1); transform: scale(1); }
       }
+
+      :host([index='0']) {
+        background-image: linear-gradient(#eb7ea9 7.5px, transparent 7.5px), linear-gradient(to right, #eb7ea9 7.5px, transparent 7.5px);
+        background-size: 25px 25px;
+        background-color: #f3b1cd;
+      }
+
+      :host([index='1']) {
+        background-image: repeating-conic-gradient(#bad5f0 0% 25%, #07b0da 0% 50%);
+        background-position: 0 0, 25px 25px;
+        background-size: 50px 50px;
+        background-color: #07b0da;
+      }
+
+      :host([index='2']) {
+        background-image: radial-gradient(#6f59a0 2.5px, transparent 2.5px), radial-gradient(#6f59a0 2.5px, transparent 2.5px);
+        background-size: 19px 19px;
+        background-position: 0 0, 9.5px 9.5px;
+        background-color: #b0abcc;
+      }
+
+      :host([index='3']) {
+        background-image: repeating-linear-gradient(45deg, transparent, transparent 19px, #96c8ad 19px, #96c8ad 38px);
+        background-color: #c2d5a8;
+      }
     `;
   }
 
@@ -57,7 +81,7 @@ export class BoxButton extends LitElement {
 
   render() {
     return html`
-      I'm : ${this.index}
+
     `;
   }
 
--- src/S1m0nGame.js
+++ src/S1m0nGame.js
@@ -1,5 +1,16 @@
 import {html, css, LitElement} from 'lit';
 import {when} from 'lit/directives/when.js';
+import {
+  provideFluentDesignSystem,
+  fluentButton,
+  fluentBadge,
+  fluentCard
+} from "@fluentui/web-components";
+
+
+provideFluentDesignSystem().register(fluentCard());
+provideFluentDesignSystem().register(fluentBadge());
+provideFluentDesignSystem().register(fluentButton());
 
 export class S1m0nGame extends LitElement {
   static get styles() {
@@ -7,17 +18,43 @@ export class S1m0nGame extends LitElement {
       :host {
         height: 450px;
         width: 350px;
-        background-color: beige;
         display: block;
+        font-family: system-ui,serif;
+        font-weight: bold;
       }
 
-      div {
-        height: auto;
+      #board {
+        height: 100%;
         display: grid;
         grid-template-columns: auto auto;
+        place-items: center;
+        row-gap: 10px;
+        margin: 5px 0px;
+        justify-content: space-around;
         justify-items: center;
+        align-content: center;
+      }
+
+      fluent-card {
+        padding: 1em;
+      }
+
+      #start {
+        height: 100%;
+        width: auto;
+        display: flex;
+        align-content: center;
         align-items: center;
-        row-gap: 20px;
+        justify-content: center;
+      }
+
+      #display {
+        display: flex;
+        justify-content: space-between;
+        align-content: center;
+        flex-wrap: wrap;
+        flex-direction: row;
+        align-items: baseline;
       }
     `;
   }
@@ -76,50 +113,64 @@ export class S1m0nGame extends LitElement {
     this.whoIsTurn = 's1m0n';
   }
 
-   async __newTurn() {
-     await this.updateComplete;
-     this.onHighlight = -1;
-     this.playerStep = 0;
-     if (this.whoIsTurn === 's1m0n') {
-       this.addToSequence();
-       await this.updateComplete;
-       this._animateCurrentBox();
-     }
+  async __newTurn() {
+    await this.updateComplete;
+    this.onHighlight = -1;
+    this.playerStep = 0;
+    if (this.whoIsTurn === 's1m0n') {
+      this.addToSequence();
+      await this.updateComplete;
+      this._animateCurrentBox();
+    }
 
-   }
+  }
 
-   async __changeTurn() {
-     await this.updateComplete;
-     this.onHighlight = -1;
-     this.playerStep = 0;
-     setTimeout(() => {
-       this.whoIsTurn = this.whoIsTurn === 's1m0n' ? 'player' : 's1m0n';
-       if (this.whoIsTurn === 's1m0n') {
-         this.addToSequence();
-       }
-     },0);
+  async __changeTurn() {
+    await this.updateComplete;
+    this.onHighlight = -1;
+    this.playerStep = 0;
+    setTimeout(() => {
+      this.whoIsTurn = this.whoIsTurn === 's1m0n' ? 'player' : 's1m0n';
+      if (this.whoIsTurn === 's1m0n') {
+        this.addToSequence();
+      }
+    }, 0);
 
-   }
+  }
 
   async _animateCurrentBox() {
     if (!this.isAnimate) {
+      this.__playSound(this.whoIsTurn);
       setTimeout(() => {
         this.onHighlight = this.sequence[this.playerStep];
         this.playerStep++;
         this.requestUpdate();
-      },0);
+      }, 0);
       await this.updateComplete;
     }
+  }
+
+  __playSound(type) {
+    const gallery = {
+      'gameOver': 'https://cdn.pixabay.com/download/audio/2022/02/11/audio_7f0bf4cdc0.mp3?filename=buzzer-or-wrong-answer-20582.mp3',
+      's1m0n': 'https://cdn.pixabay.com/download/audio/2022/03/26/audio_515cd38fa2.mp3?filename=slow-wind-sound-effect-108401.mp3',
+      'player': 'https://cdn.pixabay.com/download/audio/2022/01/07/audio_ea449d6cea.mp3?filename=start-13691.mp3'
+    }
+    const audio = new Audio(gallery[type]);
+    audio.loop = false;
+    audio.play();
   }
 
   addToSequence() {
     const nextStep = Math.floor(Math.random() * this.options.length);
     this.sequence.push(nextStep);
-
   }
 
   compareMove(idColor) {
     if (this.sequence[this.playerStep] !== idColor) {
+      setTimeout(() => {
+        this.__playSound('gameOver');
+      });
       this.isOver = true;
     }
     this.playerStep++;
@@ -127,42 +178,47 @@ export class S1m0nGame extends LitElement {
   }
 
   updated(_changedProperties) {
-    if (!this.isAnimate && !this.isOver && (_changedProperties.has('whoIsTurn')  || _changedProperties.has('isAnimate') )) {
-        if (this.sequence.length !== 0 && (this.sequence.length === this.playerStep)) {
-          this.__changeTurn();
-        }
-        if (this.sequence.length === 0) {
-          this.__newTurn();
-        }
-        if (this.whoIsTurn === 's1m0n' && this.playerStep < this.sequence.length) {
-          this._animateCurrentBox();
-        }
+    if (!this.isAnimate && !this.isOver && (_changedProperties.has('whoIsTurn') || _changedProperties.has('isAnimate'))) {
+      if (this.sequence.length !== 0 && (this.sequence.length === this.playerStep)) {
+        this.__changeTurn();
+      }
+      if (this.sequence.length === 0) {
+        this.__newTurn();
+      }
+      if (this.whoIsTurn === 's1m0n' && this.playerStep < this.sequence.length) {
+        this._animateCurrentBox();
+      }
     }
   }
-
   render() {
     return html`
-      <h2>S1m0n Game</h2>
-      <p>Turn: ${this.whoIsTurn}</p>
-      <p>IsOver: ${this.isOver}</p>
+      <fluent-card>
       ${when(this.isOver, () => html`
-        <button @click=${this.__start}>Start</button>
+        <div id="start">
+          <fluent-button appearance="accent" @click=${this.__start}>Start</fluent-button>
+        </div>
       `, () => html`
-        <div>
+        <div id="display">
+          <fluent-badge appearance="${this.whoIsTurn === 's1m0n' ? 'accent': 'neutral'}">S1m0n</fluent-badge>
+          <span>${this.sequence.length - 1}</span>
+          <fluent-badge appearance=${this.whoIsTurn === 'player' ? 'accent': 'neutral'}>You</fluent-badge>
+        </div>
+        <div id="board">
           ${this.options.map((opt, i) => html`
             <box-button
               background=${opt}
               index=${i}
               enabled=${this.whoIsTurn !== 's1m0n'}
               highlight=${i === this.onHighlight}
-              @click=${()=> {
+              @click=${() => {
                 if (this.whoIsTurn !== 's1m0n' && !this.isAnimate) {
+                  this.__playSound(this.whoIsTurn);
                   this.onHighlight = i;
                   this.compareMove(i);
                   this.requestUpdate();
                 }
               }}
-              @webkitAnimationStart=${() =>{
+              @webkitAnimationStart=${() => {
                 this.isAnimate = true;
                 this.requestUpdate();
               }}
@@ -175,6 +231,7 @@ export class S1m0nGame extends LitElement {
           `)}
         </div>
       `)}
+      </fluent-card>
     `;
   }
 }
